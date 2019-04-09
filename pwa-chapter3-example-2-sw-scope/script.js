bb87672da9b096c56aad905ffc7228995f2cf1c9
Alfio P <alfio.p78@gmail.com>
2019-04-09 12:22:44 +0200

    ADD - Comment

--- pwa-chapter3-example-2-sw-scope/script.js
+++ pwa-chapter3-example-2-sw-scope/script.js
@@ -1,4 +1,9 @@
 (() => {
+
+  // Lo scope del service-worker influenzerà su tutta l'applicazione se definito nella root oppure in specifiche cartelle
+  // influenzerà solo le sottocartelle, in base a dove è posizionato lo script
+
+  // qui viene specificato lo scope in 'trivia' e come secondo parametro si può specificare solo una sottocartella di trivia
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
     navigator.serviceWorker.register('scripts/trivia/service-worker.js', {scope: 'scripts'}).then((registration) => {
